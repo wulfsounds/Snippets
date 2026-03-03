0eb1beb91dd69aab1aa18f6472f0b285e8e08a32
Dev Wulf <93016392+wulfsounds@users.noreply.github.com>
2026-03-03 09:22:26 -0600

    Create number-counter.js

---
+++ number-counter.js
@@ -0,0 +1,20 @@
+<script src="https://cdn.jsdelivr.net/npm/@srexi/purecounterjs/dist/purecounter_vanilla.js"></script>
+    <script>
+       new PureCounter({
+   
+   
+    selector: '.uptick-text_ani',	
+    start: 0, 			            
+    end: 82, 			           
+    duration: 2, 	               
+    delay: 20, 			            
+    once: true, 		           
+    repeat: false, 		            
+    decimals: 0, 		            
+    legacy: true,                   
+    filesizing: false, 	            
+    currency: false, 	            
+    separator: false, 	          
+});
+
+</script>
